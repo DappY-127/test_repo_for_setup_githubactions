8c21d1b9c799ff4c39413524d79fefb9b9b95372
Paul <paul.mshtk.707@gmail.com>
2023-04-02 10:22:08 +0300

    update conftest.py, corrected the missing letter

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from selnium import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service 
 from webdriver_manager.chrome import ChromeDriverManager
